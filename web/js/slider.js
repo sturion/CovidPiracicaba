912a7458f7ec9dcb0a52eb94efe650d1e2acb077
CleberAugustz <cleber.augustz@hotmail.com>
2021-01-02 17:58:20 -0300

    Integração com API, permitindo atualização automatizada dos dados. Add dados.js, que contém toda a lógica para acesso as informações e atualização das estáticas. Modificações nos arquivos index.html e modularização do arquivo main, devido a necesssidade de assincronismo com a API antes mesmo de gerar as estatísticas.

--- web/js/slider.js
+++ web/js/slider.js
@@ -1,18 +1,21 @@
 // GATILHOS DA BARRA DE ISOLAMENTO
 
-document.getElementById("isolamento").addEventListener('input', changeCss);
+document.getElementById("isolamento").addEventListener("input", changeCss);
 
-var UltRegIsoSlider = document.getElementById('isolamento').value
-
-console.log(UltRegIsoSlider)
-
+var UltRegIsoSlider = document.getElementById("isolamento").value;
 
 //FUNÇÃO PARA MUDAR CSS DA BARRA DE ISOLAMENTO QUANDO DISPARADA
 
 function changeCss() {
-
     let isolamento = document.getElementById("isolamento").value;
-    let color = 'rgb(' + (250 - 3.333 * isolamento) + ',' + (0) + ',' + (3.333 * isolamento) + ')';
+    let color =
+        "rgb(" +
+        (250 - 3.333 * isolamento) +
+        "," +
+        0 +
+        "," +
+        3.333 * isolamento +
+        ")";
 
     document.getElementById("pct").style.color = color;
     document.getElementById("pct").innerHTML = `${isolamento}%`;
@@ -20,53 +23,50 @@ function changeCss() {
     document.getElementById("pctIsoLeg").innerHTML = `Taxa Simulada`;
 
     let infectadosSlider = document.getElementById("infectados").innerHTML;
-    let infectadosPrevSlider = document.getElementById("infectadosPrev").innerHTML;
+    let infectadosPrevSlider = document.getElementById("infectadosPrev")
+        .innerHTML;
 
     infectadosSlider = parseInt(infectadosSlider);
     infectadosPrevSlider = parseInt(infectadosPrevSlider);
 
     if (isolamento == UltRegIsoSlider) {
-
         document.getElementById("pct").style.color = "var(--branco)";
         document.getElementById("aviso").style.display = "block";
         document.getElementById("pctIsoLeg").innerHTML = `Ultima média reg.`;
-        document.getElementById("aviso").style.backgroundColor = "rgba(190,0,0,0.85)";
-        document.getElementById("aviso").innerHTML = ` Arraste a barra para simular outros valores`
-
-
+        document.getElementById("aviso").style.backgroundColor =
+            "rgba(190,0,0,0.85)";
+        document.getElementById(
+            "aviso"
+        ).innerHTML = ` Arraste a barra para simular outros valores`;
     }
-
 
     if (infectadosSlider == infectadosPrevSlider) {
-
         document.getElementById("aviso").style.display = "block";
-        document.getElementById("aviso").style.backgroundColor = "rgba(20,125,190,0.85)";
-        document.getElementById("aviso").innerHTML = `Taxa de Isolamento ideal`
+        document.getElementById("aviso").style.backgroundColor =
+            "rgba(20,125,190,0.85)";
+        document.getElementById("aviso").innerHTML = `Taxa de Isolamento ideal`;
     }
 
-    let hospitalizadosSlider = document.getElementById("hospitalizados").innerHTML;
-    let hospitalizadosPrevSlider = document.getElementById("hospitalizadosPrev").innerHTML;
+    let hospitalizadosSlider = document.getElementById("hospitalizados")
+        .innerHTML;
+    let hospitalizadosPrevSlider = document.getElementById("hospitalizadosPrev")
+        .innerHTML;
     let numeroLietos = document.getElementById("numeroLeitos").innerHTML;
-
 
     hospitalizadosSlider = parseInt(hospitalizadosSlider);
     hospitalizadosPrevSlider = parseInt(hospitalizadosPrevSlider);
     numeroLietos = parseInt(numeroLietos);
 
-
     let novosInfec = hospitalizadosPrevSlider - hospitalizadosSlider;
 
-    let demandaLeitos = (novosInfec) * 0.15;
-    console.log(demandaLeitos)
-
-
+    let demandaLeitos = novosInfec * 0.15;
 
     if (demandaLeitos > numeroLietos) {
-
         document.getElementById("aviso").style.display = "block";
-        document.getElementById("aviso").style.backgroundColor = "rgba(190,0,0,0.85)";
-        document.getElementById("aviso").innerHTML = `Possível insuficiência de UTI's em 5 dias`
-
+        document.getElementById("aviso").style.backgroundColor =
+            "rgba(190,0,0,0.85)";
+        document.getElementById(
+            "aviso"
+        ).innerHTML = `Possível insuficiência de UTI's em 5 dias`;
     }
-
 }
